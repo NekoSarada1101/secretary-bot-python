09a1b98bbdb82290c1cd53d1c5f1e2ad8d3192ea
Sarada1101 <ryoyukikazu11@gmail.com>
2020-09-05 00:14:26 +0900

    create: secretaryBotのカレンダー機能

---
+++ google_calendar.py
@@ -0,0 +1,120 @@
+import googleapiclient.discovery
+from datetime import datetime, timedelta, timezone
+from setting_secret import *
+
+
+def fetch_all_calendar_data(date):
+    my_cal = fetch_today_events(MY_MAIL, date)
+    work_cal = fetch_today_events(WORK_MAIL, date)
+    school_cal = fetch_today_events(SCHOOL_MAIL, date)
+    timetable_cal = fetch_today_events(TIMETABLE_MAIL, date)
+
+    data = calendar_json(my_cal, work_cal, school_cal, timetable_cal, date.strftime("%m月%d日"))
+
+    return data
+
+
+def fetch_today_events(calendar_id, date):
+    service = googleapiclient.discovery.build('calendar', 'v3', credentials=CREDENTIALS)
+
+    min = date.replace(hour=0, minute=0, second=0).strftime("%Y-%m-%dT%H:%M:%S%z")
+    max = date.replace(hour=23, minute=59, second=59).strftime("%Y-%m-%dT%H:%M:%S%z")
+
+    page_token = None
+    while True:
+        events = service.events().list(calendarId=calendar_id, pageToken=page_token, timeMin=min, timeMax=max,
+                                       singleEvents=True, orderBy="startTime").execute()
+
+        list_text = ""
+        for event in events['items']:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+
+            if len(start) == 10:
+                start = datetime.strptime(start, "%Y-%m-%d").strftime("%m/%d ")
+                list_text += start
+            else:
+                start = datetime.strptime(start, "%Y-%m-%dT%H:%M:%S%z").strftime("%m/%d %H:%M-")
+                end = event['end'].get('dateTime', event['end'].get('date'))
+                end = datetime.strptime(end, "%Y-%m-%dT%H:%M:%S%z").strftime("%H:%M ")
+                list_text += start + end
+
+            list_text += "`" + event['summary'] + "`" + "\n"
+
+        page_token = events.get('nextPageToken')
+        if not page_token:
+            break
+
+    return list_text
+
+
+def calendar_json(my_cal, work_cal, school_cal, timetable_cal, today):
+    data = {
+        "response_type": "ephemeral",
+        "text": today + "の予定をお知らせします。",
+        "attachments": [
+            {
+                "fallback": "my",
+                "color": "FF0000",
+                "title": "自分の予定",
+                "text": my_cal
+            },
+            {
+                "fallback": "recruit",
+                "color": "00BFFF",
+                "title": "インターンの予定",
+                "text": work_cal
+            },
+            {
+                "fallback": "school",
+                "color": "FFFF00",
+                "title": "学校の予定",
+                "text": school_cal
+            },
+            {
+                "fallback": "timetable",
+                "color": "FFFFFF",
+                "title": "時間割の予定",
+                "text": timetable_cal
+            }
+        ]
+    }
+    return data
+
+
+def date_pick_json():
+    JST = timezone(timedelta(hours=+9), 'JST')
+    today = datetime.now(JST).strftime("%Y-%m-%d")
+    data = {
+        "response_type": "ephemeral",
+        "attachments": [
+            {
+                "color": "FFFFFF",
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "何日の予定を確認しますか？",
+                        }
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "予定を確認したい日付を選択してください"
+                        },
+                        "accessory": {
+                            "type": "datepicker",
+                            "initial_date": today,
+                            "action_id": "date",
+                            "placeholder": {
+                                "type": "plain_text",
+                                "text": "Select a date",
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+    return data
---
+++ main.py
@@ -0,0 +1,96 @@
+from setting_secret import *
+import google_calendar
+import json
+import requests
+from datetime import datetime, timedelta, timezone
+
+
+def do_post(e):
+    token = e.form.get('token')
+    payload = None
+    try:
+        payload = json.loads(e.form.get("payload"))
+        print(payload)
+    except Exception as error:
+        print(error)
+
+    if token != SLACK_TOKEN and payload['token'] != SLACK_TOKEN:
+        raise Exception("not allowed token")
+
+    if token == SLACK_TOKEN:
+        data = {
+            "response_type": "ephemeral",
+            "attachments": [
+                {
+                    "color": "FFFFFF",
+                    "blocks": [
+                        {
+                            "type": "section",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "何かご用でしょうか？",
+                            }
+                        },
+                        {
+                            "type": "actions",
+                            "elements": [
+                                {
+                                    "type": "button",
+                                    "text": {
+                                        "type": "plain_text",
+                                        "text": "予定を教えて"
+                                    },
+                                    "value": "calendar"
+                                },
+                                {
+                                    "type": "button",
+                                    "text": {
+                                        "type": "plain_text",
+                                        "text": "天気を教えて"
+                                    },
+                                    "value": "weather"
+                                },
+                                {
+                                    "type": "button",
+                                    "text": {
+                                        "type": "plain_text",
+                                        "text": "テーマを変えたい"
+                                    },
+                                    "value": "theme"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+
+    else:
+        value = None
+        action_id = None
+        # valueの取得、判定
+        try:
+            value = payload["actions"][0]["value"]
+        except KeyError as error:
+            print(error)
+
+        if value == "calendar":
+            data = google_calendar.date_pick_json()
+
+        # action_idの取得、判定
+        try:
+            action_id = payload['actions'][0]['action_id']
+        except KeyError as error:
+            print(error)
+
+        if action_id == "date":
+            JST = timezone(timedelta(hours=+9), 'JST')
+            today = datetime.now(JST)
+            pick_date = datetime.strptime(payload["actions"][0]["selected_date"], "%Y-%m-%d")
+            pick_date = today.replace(year=pick_date.year, month=pick_date.month, day=pick_date.day)
+
+            data = google_calendar.fetch_all_calendar_data(pick_date)
+
+    json_data = json.dumps(data).encode("utf-8")
+    requests.post(HISHO_URL, json_data)
+    return ""
