c7e4fc5f1a914dafa68a867d0e02440b47ee6db2
Sarada1101 <ryoyukikazu11@gmail.com>
2020-09-05 14:39:51 +0900

    update: pub/subからのメッセージで分岐

--- trigger.py
+++ trigger.py
@@ -2,14 +2,24 @@ from datetime import datetime, timedelta, timezone
 import google_calendar
 import json
 import requests
+import base64
 from setting_secret import *
 
 
 def morning_trigger(event, context):
+    if 'data' in event:
+        frequency = base64.b64decode(event['data']).decode('utf-8')
+    else:
+        frequency = None
+
     JST = timezone(timedelta(hours=+9), 'JST')
     today = datetime.now(JST)
 
-    data = google_calendar.fetch_all_calendar_data(today)
+    if frequency == "day":
+        data = google_calendar.fetch_day_calendar_data(today)
+
+    elif frequency == "week":
+        data = google_calendar.fetch_week_calendar_data(today)
 
     json_data = json.dumps(data).encode("utf-8")
     requests.post(DIARY_URL, json_data)
