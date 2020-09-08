5a96cccdf87afd1299f55a356a583c8795d54a22
Sarada1101 <ryoyukikazu11@gmail.com>
2020-09-08 12:00:48 +0900

    create: fetch_weather_dataメソッド

--- weather.py
+++ weather.py
@@ -1,18 +1,58 @@
 import requests
+import json
+from datetime import datetime
+from typing import List
 from setting_secret import *
 
-def fetch_weather_data():
 
+def fetch_weather_data(when: str) -> dict:
+    url = ("https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&exclude=current,minutely,"
+           "hourly&lang=ja&units=metric" % (
+               LAT,
+               LON,
+               OPEN_WEATHER_API_KEY))  # type: str
+    response = json.loads(requests.get(url).text)  # type: dict
 
+    index = []  # type: List[int]
+    if when == "today":
+        index = [0]
+    elif when == "tomorrow":
+        index = [1]
+    elif when == "week":
+        index = [0, 1, 2, 3, 4, 5, 6, 7]
 
+    dt = []  # type: List[str]
+    weather = []  # type: List[str]
+    image = []  # type: List[str]
+    day_temp = []  # type: List[str]
+    max_temp = []  # type: List[str]
+    min_temp = []  # type: List[str]
+    wind_speed = []  # type: List[str]
+    pop = []  # type: List[str]
+    for i in index:
+        weather.append(response["daily"][i]["weather"][0]["description"])
+        image.append(response["daily"][i]["weather"][0]["icon"])
 
-def weather():
-    url = ("https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&exclude=minutely,hourly" % (LAT,
-                                                                                                               LON,
-                                                                                                               OPEN_WEATHER_API_KEY))
+        if when == "today" or when == "tomorrow":
+            max_temp.append(str(round(response["daily"][i]["temp"]["max"], 1)) + "°C")
+            min_temp.append(str(round(response["daily"][i]["temp"]["min"], 1)) + "°C")
+            wind_speed.append(str(response["daily"][i]["wind_speed"]) + "m/h")
+            pop.append(str(int(response["daily"][i]["pop"] * 100)) + "%")
 
-    response = requests.get(url)
-    print(response.text)
+        elif when == "week":
+            dt.append(datetime.fromtimestamp(response["daily"][i]["dt"]).strftime("%m/%d"))
+            day_temp.append(str(round(response["daily"][i]["temp"]["day"], 1)) + "°C")
+
+    data = {}  # type: dict
+    if when == "today":
+        data = day_weather_json("今日", weather, image, max_temp, min_temp, wind_speed, pop)
+    elif when == "tomorrow":
+        data = day_weather_json("明日", weather, image, max_temp, min_temp, wind_speed, pop)
+    elif when == "week":
+        data = week_weather_json(dt, weather, day_temp)
+
+    return data
+
 
 
 def weather_menu_json():
