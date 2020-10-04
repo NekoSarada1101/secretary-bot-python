ff618b6856e204c173dcdcbb04fbd7806b31fc88
Sarada1101 <ryoyukikazu11@gmail.com>
2020-10-04 20:45:38 +0900

    update: イベント通知とデバッグ

--- trigger.py
+++ trigger.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta, timezone
 import google_calendar
 import weather
+import notification_events
 import json
 import requests
 import base64
@@ -24,8 +25,20 @@ def morning_trigger(event: dict, context) -> str:
     # 今日の天気
     weather_data = weather.fetch_weather_data("today")  # type: dict
 
-    json_data = json.dumps(calendar_data).encode("utf-8")  # type json
+    # 技術イベント通知
+    connpass_data = notification_events.fetch_tech_events("https://connpass.com/explore/ja.atom")  # type: dict
+    techplay_data = notification_events.fetch_tech_events("https://rss.techplay.jp/event/w3c-rss-format/rss.xml")  # type: dict
+
+    print(weather_data)
+    json_data = json.dumps(weather_data).encode("utf-8")  # type json
     requests.post(DIARY_URL, json_data)
-    json_data = json.dumps(weather_data).encode("utf-8")
+    print(calendar_data)
+    json_data = json.dumps(calendar_data).encode("utf-8")
     requests.post(DIARY_URL, json_data)
+    print(connpass_data)
+    json_data = json.dumps(connpass_data).encode("utf-8")
+    requests.post(TECH_EVENT_URL, json_data)
+    print(techplay_data)
+    json_data = json.dumps(techplay_data).encode("utf-8")
+    requests.post(TECH_EVENT_URL, json_data)
     return ""
